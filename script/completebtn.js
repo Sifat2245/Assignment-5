152dca35698e0e9d2e91301515ef50d777cf2bba
Sifat2245 <Sifat2245@users.noreply.github.com>
2025-03-02 02:17:30 +0600

    addec functionality to the required fields

--- script/completebtn.js
+++ script/completebtn.js
@@ -1,29 +1,49 @@
 const completeBtns = document.querySelectorAll('.complete-btn');
+const activityTaskContainer = document.getElementById('task-message-container')
 
-
-for(let i = 0; i < completeBtns.length; i++){
+for (let i = 0; i < completeBtns.length; i++) {
     // console.log(completeBtn[i]);
     const completeBtn = completeBtns[i];
-    completeBtn.addEventListener('click', function(){
+    completeBtn.addEventListener('click', function () {
         alert('Board updated Successfully');
-       let taskDecrease = document.getElementById('task-decreased').innerText;
+        let taskDecrease = document.getElementById('task-decreased').innerText;
         let countedDecrease = parseInt(taskDecrease)
-       let taskIncrease = document.getElementById('task-increased').innerText;
-       let countedIncrease = parseInt(taskIncrease);
+        let taskIncrease = document.getElementById('task-increased').innerText;
+        let countedIncrease = parseInt(taskIncrease);
 
 
-    countedDecrease = taskDecrease - 1;
-                document.getElementById('task-decreased').innerText = countedDecrease;
-              
-                let count = countedIncrease;
-                count++;
-                // console.log(count);
-                document.getElementById('task-increased').innerText = count;
+        countedDecrease = taskDecrease - 1;
+        document.getElementById('task-decreased').innerText = countedDecrease;
 
-                completeBtn.setAttribute('disabled', true);
-                completeBtn.style.backgroundColor = '#a2adf5'
+        let count = countedIncrease;
+        count++;
+        // console.log(count);
+        document.getElementById('task-increased').innerText = count;
 
+        completeBtn.setAttribute('disabled', true);
+        completeBtn.style.backgroundColor = '#a2adf5'
+        if (countedDecrease === 0) {
+            alert('congrats!!! You have completed all the current task')
+        }
+
+        let heading = completeBtn.closest('.task-card');
+        if (heading) {
+            const h1 = heading.querySelector('h1').innerText;
+            // console.log(h1)
+            let taskMessagep = document.createElement('p');
+            let time = new Date();
+            let formatedTime = time.toLocaleTimeString();
+            taskMessagep.innerText = `you have completed the task ${h1} at ${formatedTime}`
+            // console.log(taskMessagep);
+            taskMessagep.classList.add('message')
+            activityTaskContainer.appendChild(taskMessagep)
+        }
 
     }
     )
 }
+
+
+document.getElementById('clear-history-btn').addEventListener('click', function(){
+    activityTaskContainer.innerHTML = '';
+})
